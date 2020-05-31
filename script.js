d3281989796f368f5445c0363400c452bc531cb1
troutbum <64603749+andy.pedonti@users.noreply.github.com>
2020-05-31 19:16:32 -0400

    another try

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
@@ -7,7 +7,8 @@ function writePassword() {
   const passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+ 
+ 
 }
 
 
@@ -16,8 +17,10 @@ generateBtn.addEventListener("click", writePassword);
 
 function writePassword () {
   const birthCity = prompt("what city were you born in");
-
-  confirm = prompt("what was the name of your first pet?");
-  confirm = prompt("what is your favorite holiday?")
+    console.log(birthCity)
+  const firstPet = prompt("what was the name of your first pet?");
+    console.log(firstPet)
+  const favoriteHoliday = prompt("what is your favorite holiday?");
+  console.log(favoriteHoliday)
+  console.log([birthCity + firstPet + favoriteHoliday])
 }
-
